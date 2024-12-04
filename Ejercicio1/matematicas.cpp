8065db8d69b3f9b3178332902eb1d3a7452a1d38
gabriel140282 <gkaakmor@myuax.com>
2024-12-04 17:52:54 +0100

    V0.2

--- Ejercicio1/matematicas.cpp
+++ Ejercicio1/matematicas.cpp
@@ -1,4 +1,21 @@
 #include "matematicas.h"
+#include <iostream>
 
+namespace Matematicas {
+    int suma(int a, int b) {
+        return a + b;
+    }
 
+    int resta(int a, int b) {
+        return a - b;
+    }
+}
+
+void suma_numeros(int num1, int num2) {
+    std::cout<<"La suma de "<<num1<<" y "<<num2<<" es: "<< Matematicas::suma(num1, num2) << std::endl;
+}
+
+void resta_numeros(int num1, int num2) {
+    std::cout<<"La resta de "<<num1<<" y "<<num2<<" es: "<< Matematicas::resta(num1, num2) << std::endl;
+}
 
--- Ejercicio1/matematicas.h
+++ Ejercicio1/matematicas.h
@@ -1,6 +1,12 @@
 #ifndef MATEMATICAS_H
 #define MATEMATICAS_H
 
+namespace Matematicas {
+    int suma(int a, int b);
+    int resta(int a, int b);
+}
 
+void suma_numeros(int num1, int num2);
+void resta_numeros(int num1, int num2);
 
 #endif // MATEMATICAS_H
